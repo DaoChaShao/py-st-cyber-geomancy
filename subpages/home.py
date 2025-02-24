c28956115124766b7ceeb7f1bd25b91655b25984
DaoChaShao <27882272@qq.com>
2025-02-25 01:12:04 +1300

    feat: update home.py to enhance user guidance on model usage and performance

--- subpages/home.py
+++ subpages/home.py
@@ -14,8 +14,10 @@ with expander("Introduction", expanded=True):
     caption("Cyber Geomancy is a project that uses the Word2Vec model to analyze the similarity between words.")
     caption("The project is based on the Gensim library and the Streamlit framework.")
     caption("The project is a demonstration of the use of pre-trained models in the field of NL Processing.")
-    caption("If you use the models of **cc.en.300.vec** and **cc.zh.300.vec** to test the similarity, \
+    caption("If you use the models of **cc.en.300.vec** and **cc.zh.300.vec** to test the similarity locally, \
     you should be patient, because you have to take approximately 5 minutes for every single round.")
+    caption("However, if you use the models of **en_core_web_lg** and **zh_core_web_md** to test locally, \
+            you can get the result in a few seconds.")
 
 empty_message: empty = empty()
 
