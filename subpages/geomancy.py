3f510a894261bda6eaa11f3f4252e9f7bb052d6a
DaoChaShao <27882272@qq.com>
2025-02-24 20:46:36 +1300

    feat: enhance geomancy.py to check model existence and similarity

--- subpages/geomancy.py
+++ subpages/geomancy.py
@@ -4,12 +4,28 @@
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
 # @File     :   geomancy.py
-# @Desc     :   
+# @Desc     :
 
-def main() -> None:
-    """ Main Function """
-    pass
+from streamlit import empty, sidebar, button
 
+from utilis.tools import params, is_model, similarity_checker, Timer
 
-if __name__ == "__main__":
-    main()
+empty_messages: empty = empty()
+
+model_name, vocab_x, vocab_y = params()
+
+if is_model(model_name):
+    empty_messages.info(f"The model **{model_name.upper()}** exists.")
+else:
+    empty_messages.error(f"The model **{model_name.upper()}** does not exist.")
+
+with sidebar:
+    check = button("Check Similarity", type="primary", help="Check the similarity between two words.")
+
+if check:
+    empty_messages.warning("Please wait for the result...")
+
+    with Timer("Word Vector Similarity") as timer:
+        similarity_checker(model_name, vocab_x, vocab_y)
+
+    empty_messages.success(timer)
