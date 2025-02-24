32d3498aee746a4e89971b6cd5abe4b887fda0ba
DaoChaShao <27882272@qq.com>
2025-02-25 01:07:56 +1300

    feat: implement similarity checking using SpaCy and add model management functions

--- utilis/tools.py
+++ utilis/tools.py
@@ -10,6 +10,8 @@ from enum import unique, StrEnum
 from gensim.models import KeyedVectors
 from os import path
 from pandas import DataFrame
+from spacy.cli import download
+from spacy.util import get_package_path
 from streamlit import (sidebar, header, selectbox, caption, text_input,
                        empty, markdown, bar_chart, data_editor)
 from time import perf_counter
@@ -52,8 +54,8 @@ def is_lang(model_name: str, vocab_x: str, vocab_y: str) -> bool:
         return False
 
 
-def similarity_checker(model_name: str, vocab_x: str, vocab_y: str, messages: empty,
-                       precision: int = 2) -> float | None:
+def similarity_gensim(model_name: str, vocab_x: str, vocab_y: str, messages: empty,
+                      precision: int = 2) -> float | None:
     """ Check the similarity between two words """
     model_path = f"models/{model_name}.vec"
 
@@ -68,10 +70,10 @@ def similarity_checker(model_name: str, vocab_x: str, vocab_y: str, messages: em
             return similarity
         elif word_x not in model.key_to_index:
             print(f"{word_x} is not in the vocabulary.")
-            markdown(f"**{word_x}** is not in the vocabulary.")
+            messages.warning(f"**{word_x}** is not in the vocabulary.")
         else:
             print(f"**{word_y}** is not in the vocabulary.")
-            markdown(f"**{word_y}** is not in the vocabulary.")
+            messages.warning(f"**{word_y}** is not in the vocabulary.")
     else:
         print("The language of the model and the words are not matched.")
         messages.error("The language of the model and the words are not matched.")
@@ -108,7 +110,7 @@ class Timer(object):
             return f"{self._description} is failed to cal."
 
 
-def params() -> tuple[str, str, str]:
+def params_gensim() -> tuple[str, str, str]:
     with sidebar:
         header("Parameters")
         options: list = ["cc.en.300", "cc.zh.300"]
@@ -139,7 +141,7 @@ class Colour(StrEnum):
     GRAY: str = "#525352"
 
 
-def plot_similarity(value: float, vocab_x: str, vocab_y: str) -> None:
+def plot_similarity_gensim(value: float, vocab_x: str, vocab_y: str) -> None:
     # Prepare the data for the bar chart
     data = DataFrame({"similarity": [value]})
     data_editor(data, disabled=True, hide_index=True, use_container_width=True)
@@ -156,6 +158,79 @@ def plot_similarity(value: float, vocab_x: str, vocab_y: str) -> None:
         result: str = "Completely Opposite"
         colour: str = Colour.RED
         geo: str = "You are FAR AWAY from each other!"
+
+    title: str = f"The Similarity Result between {vocab_x} and {vocab_y} is {result}"
+    markdown(f"##### {title}")
+    bar_chart(data, x=None, color=colour, use_container_width=True)
+    markdown(f"##### {geo}")
+
+
+def params_scapy():
+    with sidebar:
+        header("Parameters")
+        options: list = [
+            "en_core_web_lg", "zh_core_web_lg",
+            "en_core_web_md", "zh_core_web_md",
+            "en_core_web_sm", "zh_core_web_sm"
+        ]
+        model_name: str = selectbox("Model Name", options, index=0, help="Select the pre-trained model.")
+        if is_model(model_name):
+            caption(f"The size of the model {model_name} is: {model_size_getter(model_name)}.")
+        else:
+            caption("The model does **NOT** exist!")
+
+        vocab_x: str = text_input("Enter One Name", value="Tom",
+                                  placeholder="Enter One Name",
+                                  help="Input the first word.")
+        caption(f"The first word you entered is: **{vocab_x}**.")
+        vocab_y: str = text_input("Enter Another Name", value="Jerry",
+                                  placeholder="Enter Another Name",
+                                  help="Input the second word.")
+        caption(f"The second word you entered is: **{vocab_y}**.")
+
+        return model_name, vocab_x, vocab_y
+
+
+def model_checker(model_name: str, message: empty):
+    if not get_package_path(model_name):
+        download(model_name)
+        message.info(f"Model **{model_name.upper()}** not found. Downloading...")
+        print(f"Model **{model_name.upper()}** not found. Downloading...")
+    else:
+        message.info(f"Model **{model_name.upper()}** already installed.")
+        print(f"Model **{model_name.upper()}** already installed.")
+
+
+def similarity_scapy(nlp, word_x: str, word_y: str, message: empty, precision: int = 2) -> float | None:
+    if nlp.vocab[word_x].has_vector and nlp.vocab[word_y].has_vector:
+        similarity = nlp(word_x).similarity(nlp(word_y))
+        print(f"The Cosine Similarity between {word_x} and {word_y} is: {similarity:.{precision}f}")
+        return similarity
+    elif not nlp.vocab[word_x].has_vector:
+        print(f"{word_x} is not in the vocabulary.")
+        message.warning(f"**{word_x}** is not in the vocabulary.")
+    else:
+        print(f"**{word_y}** is not in the vocabulary.")
+        message.warning(f"**{word_y}** is not in the vocabulary.")
+
+
+def plot_similarity_scapy(value: float, vocab_x: str, vocab_y: str) -> None:
+    # Prepare the data for the bar chart
+    data = DataFrame({"similarity": [value]})
+    data_editor(data, disabled=True, hide_index=True, use_container_width=True)
+
+    if 0.5 <= value < 1:
+        result: str = "Completely Identical"
+        colour = Colour.GREEN
+        geo: str = "You are CLOSE to each other!"
+    elif 0 <= value < 0.5:
+        result: str = "Completely Opposite"
+        colour: str = Colour.RED
+        geo: str = "You are FAR AWAY from each other!"
+    else:
+        result: str = "UNKNOWN"
+        colour = Colour.GRAY
+        geo: str = "The situation is RARE!"
 
     title: str = f"The Similarity Result between {vocab_x} and {vocab_y} is {result}"
     markdown(f"##### {title}")
