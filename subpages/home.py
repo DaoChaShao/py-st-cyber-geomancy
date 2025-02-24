725a4fdce4f32f42c09e21c17e07048722da8b1d
DaoChaShao <27882272@qq.com>
2025-02-24 20:46:40 +1300

    feat: update home.py to include Streamlit interface and project introduction

--- subpages/home.py
+++ subpages/home.py
@@ -6,10 +6,15 @@
 # @File     :   home.py
 # @Desc     :   
 
-def main() -> None:
-    """ Main Function """
-    pass
+from streamlit import title, divider, expander, caption, empty
 
+title("Cyber Geomancy via Names")
+divider()
+with expander("Introduction", expanded=True):
+    caption("Cyber Geomancy is a project that uses the Word2Vec model to analyze the similarity between words.")
+    caption("The project is based on the Gensim library and the Streamlit framework.")
+    caption("The project is a demonstration of the use of pre-trained models in the field of NL Processing.")
 
-if __name__ == "__main__":
-    main()
+empty_message: empty = empty()
+
+empty_message.info("You can test the similarity between words in the Geomancy page.")
