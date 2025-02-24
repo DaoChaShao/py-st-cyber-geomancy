6b26f11f79efc3c956308848aa73dc25794958c6
DaoChaShao <27882272@qq.com>
2025-02-24 21:36:43 +1300

    feat: enhance tools.py with improved similarity checker and visualization features

--- utilis/tools.py
+++ utilis/tools.py
@@ -6,10 +6,12 @@
 # @File     :   tools.py
 # @Desc     :   
 
+from enum import unique, StrEnum
 from gensim.models import KeyedVectors
 from os import path
+from pandas import DataFrame
 from streamlit import (sidebar, header, selectbox, caption, text_input,
-                       markdown)
+                       empty, markdown, bar_chart, data_editor)
 from time import perf_counter
 
 
@@ -37,11 +39,10 @@ def model_size_getter(model_name: str) -> str:
         out: str = f"{size / 1024 ** 2:.2f} MB"
     else:
         out: str = f"{size / 1024 ** 3:.2f} GB"
-
     return out
 
 
-def similarity_checker(model_name: str, vocab_x: str, vocab_y: str):
+def similarity_checker(model_name: str, vocab_x: str, vocab_y: str, presision: int = 2) -> float | None:
     """ Check the similarity between two words """
     model_path = f"models/{model_name}.vec"
 
@@ -51,16 +52,20 @@ def similarity_checker(model_name: str, vocab_x: str, vocab_y: str):
 
     if word_x in model.key_to_index and word_y in model.key_to_index:
         similarity = model.similarity(word_x, word_y)
-        print(f"The Cosine Similarity between {word_x} and {word_y} is: {similarity:.5f}")
+        print(f"The Cosine Similarity between {word_x} and {word_y} is: {similarity:.{presision}f}")
+        return similarity
     elif word_x not in model.key_to_index:
-        print(f"'{word_x}' is not in the vocabulary.")
+        print(f"{word_x} is not in the vocabulary.")
+        markdown(f"**{word_x}** is not in the vocabulary.")
     else:
-        print(f"'{word_y}' is not in the vocabulary.")
+        print(f"**{word_y}** is not in the vocabulary.")
+        markdown(f"**{word_y}** is not in the vocabulary.")
 
 
 class Timer(object):
 
-    def __init__(self, description: str, precision: int = 5):
+    def __init__(self, message: empty, description: str, precision: int = 5):
+        self._message = message
         self._description = description
         self._precision = precision
         self._start = None
@@ -70,6 +75,7 @@ class Timer(object):
     def __enter__(self):
         self._start = perf_counter()
         print(f"{self._description} is calculating...")
+        self._message.warning(f"{self._description} is calculating...")
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -79,12 +85,15 @@ class Timer(object):
 
     def __repr__(self):
         if self._elapsed is not None:
+            self._message.success(
+                f"{self._description} is completed, which takes {self._elapsed:.{self._precision}f} seconds.")
             return f"{self._description} takes {self._elapsed:.{self._precision}f} seconds."
         else:
+            self._message.error(f"{self._description} is failed to cal.")
             return f"{self._description} is failed to cal."
 
 
-def params():
+def params() -> tuple[str, str, str]:
     with sidebar:
         header("Parameters")
         options: list = ["cc.en.300", "cc.zh.300"]
@@ -94,9 +103,40 @@ def params():
         else:
             caption("The model does **NOT** exist!")
 
-        vocab_x: str = text_input("Enter One Name", value="Tom", help="Input the first word.")
+        vocab_x: str = text_input("Enter One Name", value="Tom",
+                                  placeholder="Enter One Name",
+                                  help="Input the first word.")
         caption(f"The first word you entered is: **{vocab_x}**.")
-        vocab_y: str = text_input("Enter Another Name", value="Jerry", help="Input the second word.")
+        vocab_y: str = text_input("Enter Another Name", value="Jerry",
+                                  placeholder="Enter Another Name",
+                                  help="Input the second word.")
         caption(f"The second word you entered is: **{vocab_y}**.")
 
         return model_name, vocab_x, vocab_y
+
+
+@unique
+class Colour(StrEnum):
+    RED: str = "#FFAAAA"
+    YELLOW: str = "#FFEEAA"
+    BLUE: str = "#88EEFE"
+    GREEN: str = "#AAFFAA"
+    GRAY: str = "#525352"
+
+
+def plot_similarity(value: float):
+    # Prepare the data for the bar chart
+    data = DataFrame({"similarity": [value]})
+    data_editor(data, disabled=True, hide_index=True, use_container_width=True)
+
+    if 0 < value < 1:
+        X: str = "Completely Identical"
+        colour = Colour.GREEN
+    elif value == 0:
+        X: str = "No Similarity"
+        colour = Colour.GRAY
+    else:
+        X: str = "Completely Opposite"
+        colour: str = Colour.RED
+
+    bar_chart(data, x_label=X, color=colour, use_container_width=True)
