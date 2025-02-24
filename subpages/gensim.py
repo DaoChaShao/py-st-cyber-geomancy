1c08b319a826d1ce2470e88511a2a35d292bed16
DaoChaShao <27882272@qq.com>
2025-02-25 01:07:36 +1300

    refactor: rename geomancy.py to gensim.py and update function imports for consistency

--- subpages/gensim.py
+++ subpages/gensim.py
@@ -3,18 +3,18 @@
 # @Time     :   2025/2/24 19:17
 # @Author   :   Shawn
 # @Version  :   Version 0.1.0
-# @File     :   geomancy.py
+# @File     :   gensim.py
 # @Desc     :
 
 from random import uniform
 from streamlit import empty, sidebar, button, spinner
 
-from utilis.tools import (params, is_model, similarity_checker,
-                          Timer, plot_similarity)
+from utilis.tools import (params_gensim, is_model, similarity_gensim,
+                          Timer, plot_similarity_gensim)
 
 empty_messages: empty = empty()
 
-model_name, vocab_x, vocab_y = params()
+model_name, vocab_x, vocab_y = params_gensim()
 
 if is_model(model_name):
     empty_messages.info(f"The model **{model_name.upper()}** exists.")
@@ -27,8 +27,8 @@ with sidebar:
 if check:
     with Timer(empty_messages, "Word Vector Similarity") as timer:
         with spinner("The relationship between names is Calculating...", show_time=True):
-            similarity: float = similarity_checker(model_name, vocab_x, vocab_y, empty_messages)
+            similarity: float = similarity_gensim(model_name, vocab_x, vocab_y, empty_messages)
             # similarity: float = uniform(-1.0, 1.0)
-            plot_similarity(similarity, vocab_x, vocab_y)
+            plot_similarity_gensim(similarity, vocab_x, vocab_y)
 
     empty_messages.success(timer)
