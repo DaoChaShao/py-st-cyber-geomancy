e80026dc8494312f5665bede21d35eb8fda1c0d1
DaoChaShao <27882272@qq.com>
2025-02-24 20:46:49 +1300

    feat: enhance tools.py with model size retrieval and Streamlit parameters interface

--- utilis/tools.py
+++ utilis/tools.py
@@ -8,7 +8,8 @@
 
 from gensim.models import KeyedVectors
 from os import path
-from streamlit import markdown
+from streamlit import (sidebar, header, selectbox, caption, text_input,
+                       markdown)
 from time import perf_counter
 
 
@@ -18,8 +19,26 @@ def is_model(model_name: str):
 
     if path.exists(model_path):
         print("The model exists!")
+        return True
     else:
         print("The model does not exist!")
+        return False
+
+
+def model_size_getter(model_name: str) -> str:
+    """ Get the size of the pre-trained model """
+    model_path = f"models/{model_name}.vec"
+    size: int = path.getsize(model_path)
+    if size < 1024:
+        out: str = f"{size} Bytes"
+    elif size < 1024 ** 2:
+        out: str = f"{size / 1024:.2f} KB"
+    elif size < 1024 ** 3:
+        out: str = f"{size / 1024 ** 2:.2f} MB"
+    else:
+        out: str = f"{size / 1024 ** 3:.2f} GB"
+
+    return out
 
 
 def similarity_checker(model_name: str, vocab_x: str, vocab_y: str):
@@ -32,7 +51,7 @@ def similarity_checker(model_name: str, vocab_x: str, vocab_y: str):
 
     if word_x in model.key_to_index and word_y in model.key_to_index:
         similarity = model.similarity(word_x, word_y)
-        print(f"The similarity between {word_x} and {word_y} is: {similarity:.5f}")
+        print(f"The Cosine Similarity between {word_x} and {word_y} is: {similarity:.5f}")
     elif word_x not in model.key_to_index:
         print(f"'{word_x}' is not in the vocabulary.")
     else:
@@ -63,3 +82,21 @@ class Timer(object):
             return f"{self._description} takes {self._elapsed:.{self._precision}f} seconds."
         else:
             return f"{self._description} is failed to cal."
+
+
+def params():
+    with sidebar:
+        header("Parameters")
+        options: list = ["cc.en.300", "cc.zh.300"]
+        model_name: str = selectbox("Model Name", options, index=0, help="Select the pre-trained model.")
+        if is_model(model_name):
+            caption(f"The size of the model {model_name} is: {model_size_getter(model_name)}.")
+        else:
+            caption("The model does **NOT** exist!")
+
+        vocab_x: str = text_input("Enter One Name", value="Tom", help="Input the first word.")
+        caption(f"The first word you entered is: **{vocab_x}**.")
+        vocab_y: str = text_input("Enter Another Name", value="Jerry", help="Input the second word.")
+        caption(f"The second word you entered is: **{vocab_y}**.")
+
+        return model_name, vocab_x, vocab_y
